39d450a0aadf2a398f74274b955569d7c193d01d
Rob Kinyon <rob.kinyon@gmail.com>
2022-01-08 16:18:22 -0500

    Remove hardcoding of a Gammaforce repository and properly pass default values

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# CHANGELOG
+
+* 0.0.2 (2022-01-08)
+
+Remove hardcoding of a Gammaforce repository and properly pass default values
+
+* 0.0.1 (2022-01-03)
+
+Initial version
--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ IMAGE=infra
 
 # TODO: Have this read from a version file in the repository.
 # TODO: Have that version file be stored the image.
-VERSION=0.0.1
+VERSION=0.0.2
 
 docker build \
   --tag "${ORGANIZATION}/${IMAGE}:${VERSION}" \
